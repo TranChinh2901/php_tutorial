1e8329cfd8bc0eb7c59e1c7796cb10b5dd537b42
TranChinh2901 <tranchinht32901@gmail.com>
2025-03-31 20:04:29 +0700

    tim kiem

--- index.php
+++ index.php
@@ -18,4 +18,29 @@ if (!isset($_SESSION['email'])) {
     </button>
 </a>
 
+<form method="POST">
+    <input type="text" name="noidung">
+    <button type="submit" name="btn">Tìm kiếm</button>
+</form>
+
+<?php
+if (isset($_POST['btn'])) {
+    $noidung = $_POST['noidung'];
+} else {
+    echo "Không có nội dung tìm kiếm!";
+}
+?>
+
+<?php
+include 'connect.php';
+
+$sql = "SELECT * FROM products WHERE name LIKE '%$noidung%'";
+$result = mysqli_query($conn, $sql);
+
+while ($row = mysqli_fetch_array($result)) {
+    echo $row['name'];
+    echo "<br>";
+}
+?>
+
 </html>
